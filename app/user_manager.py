f97c3fac93056358cb3474127b57d0bc11d72b3e
hajar <hajardakkouki@gmail.com>
2026-03-02 15:52:10 +0000

    Fix CI tests

--- app/user_manager.py
+++ app/user_manager.py
@@ -1,2 +1,21 @@
-def add_user(name):
-    return f"User {name} added"
+class UserManager:
+    def __init__(self):
+        self.users = {}
+
+    def add_user(self, username):
+        if not username:
+            raise ValueError("Le nom d'utilisateur est obligatoire.")
+        if username in self.users:
+            raise ValueError("Le nom d'utilisateur déjà existe.")
+        self.users.append(username)
+
+    def remove_user(self, username):
+        if username not in self.users:
+            raise ValueError("Le nom d'utilisateur introvable.")
+        self.users.remove(username)
+    def count_users(self):
+        return len(self.users)
+    
+    def count_total_users(users):
+        temp = 0
+        return len(users)
--- tests/test_user_manager.py
+++ tests/test_user_manager.py
@@ -1,4 +1,23 @@
-from app.user_manager import add_user
-
+import pytest
+from app.user_manager import UserManager
 def test_add_user():
-    assert add_user("Hajar") == "User Hajar added"
+    manager = UserManager()
+    manager.add_user("Adam")
+    assert manager.count_users() == 1
+    
+def test_add_existing_user():
+    manager = UserManager()
+    manager.add_user("Adam")
+    with pytest.raises(ValueError):
+        manager.add_user("Adam")
+
+def test_remove_user():
+    manager = UserManager()
+    manager.add_user("Yanis")
+    manager.remove_user("Yanis")
+    assert manager.count_users() == 0
+
+def test_remove_unknown_user():
+    manager = UserManager()
+    with pytest.raises(ValueError):
+        manager.remove_user("sarah")
