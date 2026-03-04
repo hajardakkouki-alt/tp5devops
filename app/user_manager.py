9b83ef325ef6f069dcc4082e9075592da0920a90
hajar <hajardakkouki@gmail.com>
2026-03-04 13:55:45 +0000

    Fix flake8 errors and correct UserManager logic

--- app/user_manager.py
+++ app/user_manager.py
@@ -1,21 +1,23 @@
 class UserManager:
+
     def __init__(self):
-        self.users = {}
+        self.users = []
 
     def add_user(self, username):
         if not username:
             raise ValueError("Le nom d'utilisateur est obligatoire.")
+
         if username in self.users:
-            raise ValueError("Le nom d'utilisateur déjà existe.")
+            raise ValueError("Le nom d'utilisateur existe déjà.")
+
         self.users.append(username)
 
     def remove_user(self, username):
         if username not in self.users:
-            raise ValueError("Le nom d'utilisateur introvable.")
+            raise ValueError("Le nom d'utilisateur est introuvable.")
+
         self.users.remove(username)
+
     def count_users(self):
         return len(self.users)
     
-    def count_total_users(users):
-        temp = 0
-        return len(users)
--- tests/test_user_manager.py
+++ tests/test_user_manager.py
@@ -1,15 +1,21 @@
 import pytest
+
 from app.user_manager import UserManager
+
+
 def test_add_user():
     manager = UserManager()
     manager.add_user("Adam")
     assert manager.count_users() == 1
-    
+
+
 def test_add_existing_user():
     manager = UserManager()
     manager.add_user("Adam")
+
     with pytest.raises(ValueError):
         manager.add_user("Adam")
+
 
 def test_remove_user():
     manager = UserManager()
@@ -17,7 +23,10 @@ def test_remove_user():
     manager.remove_user("Yanis")
     assert manager.count_users() == 0
 
+
 def test_remove_unknown_user():
     manager = UserManager()
+
     with pytest.raises(ValueError):
         manager.remove_user("sarah")
+        
